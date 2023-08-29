e93bac13e3332d0ef7bb68a9de13bac26398fec7
Ankit290901 <ankitchakraborty1234apd@gmail.com>
2023-08-29 19:09:33 +0530

    ankit4.js

---
+++ ankit3.js
@@ -0,0 +1,7 @@
+var headerTitle=document.getElementById('header-title');
+    var header=document.getElementById('main-header');
+    header.style.borderBottom = 'solid 3px #000' ;
+    const addItems = document.querySelector('.title');
+
+    addItems.style.color = 'green';
+    addItems.style.fontWeight = 'bold';
